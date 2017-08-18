50af3c038e9d60eeb7c86eda0a67412b191eb397
Altoveros, Ciarra <Ciarra.Altoveros@emc.com>
2017-08-18 13:02:25 -0700

    2. The Hero Editor - playing with components and injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{ hero.name }} details!</h2>
+    <div><label>id: </label>{{ hero.id }}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `
+  // {{ bound inside HTML }}: interpolation
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  name = 'Angular 2!';
+  title = 'Tour of Heroes';
+  hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
